e9838903ea4d57b2c35004377ad12c2c083ce602
wugangpeng <wugangpeng@meicai.cn>
2018-11-22 19:37:22 +0800

    增加swagger的配置

--- gate-way/src/main/java/com/wk/boot/shop/controller/TestController.java
+++ gate-way/src/main/java/com/wk/boot/shop/controller/TestController.java
@@ -1,7 +1,20 @@
 package com.wk.boot.shop.controller;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by wgp on 2018/11/22.
  */
+@RestController
+@RequestMapping("api/boot/shop/test")
 public class TestController {
+
+    @ApiOperation(value = "用于测试的方法",notes = "获取测试信息")
+    @GetMapping("/test0")
+    public Object test0(){
+        return "用于测试";
+    }
 }
